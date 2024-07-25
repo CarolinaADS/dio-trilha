045968f5bdcbb82f1e22b7c91455df793c573ebc
CarolinaADS <carolinacannabrava@gmail.com>
2024-07-25 14:50:31 -0300

    CRIANDO

---
+++ TIPOS-VARIAZ/src/tIPO.java
@@ -0,0 +1,13 @@
+public class tIPO {
+    public static void main(String[] args) throws Exception {
+    double salarioMinino = 2500;
+
+    short t numeroCurto = 1;
+    int numeroNormal1 = numeroCurto;
+ short numeroCurto2 = (short) numeroNormal1;
+
+int numero = 1;
+
+numero = 2;
+}
+} 
